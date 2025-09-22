d9ca5f67c65ea4e6c3917381f33c62c06503f7dd
Sarvesh <sarvesh.kumar.jsr@gmail.com>
2025-09-22 18:27:10 +0530

    types of component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Fbc from "./allTopics/01_typesOfComponents/Fbc";
+import Cbc from "./allTopics/01_typesOfComponents/Cbc";
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <p>I am App Component</p>
+
+      <Fbc />
+      <Cbc/>
+    </>
+  );
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 //! REACT WAY
 import { createRoot } from "react-dom/client";
+import App from "./App";
 let div = document.getElementById("root");
-createRoot(div).render(
-  <div>
-    <h1>Hello World</h1>
-    <h2>Hello</h2>
-  </div>
-);
+createRoot(div).render(<App/>);
+
+// <App></App>  --> Paired tag
+// <App /> --> Self closed tag
+
 
 //! JS WAY
 // let div = document.getElementById("root")
 // let h1 =  document.createElement("h1")
 // h1.innerText = "Hello World"
 // div.append(h1)
+    
